b9412de141665e6f13f50d72d90642e1f51f8cf7
ShucunZhao <frankabe001@gmail.com>
2024-01-29 18:31:54 +0800

    Improve the last version of chatgpt-sdk-java, add the streaming response function.

--- src/main/java/com/prj/chatgpt/IOpenAiApi.java
+++ src/main/java/com/prj/chatgpt/IOpenAiApi.java
@@ -1,12 +1,30 @@
 package com.prj.chatgpt;
 
+import com.prj.chatgpt.domain.billing.BillingUsage;
+import com.prj.chatgpt.domain.billing.Subscription;
 import com.prj.chatgpt.domain.chat.ChatCompletionRequest;
 import com.prj.chatgpt.domain.chat.ChatCompletionResponse;
+import com.prj.chatgpt.domain.edits.EditRequest;
+import com.prj.chatgpt.domain.edits.EditResponse;
+import com.prj.chatgpt.domain.embedd.EmbeddingRequest;
+import com.prj.chatgpt.domain.embedd.EmbeddingResponse;
+import com.prj.chatgpt.domain.files.DeleteFileResponse;
+import com.prj.chatgpt.domain.files.UploadFileResponse;
+import com.prj.chatgpt.domain.images.ImageRequest;
+import com.prj.chatgpt.domain.images.ImageResponse;
+import com.prj.chatgpt.domain.other.OpenAiResponse;
 import com.prj.chatgpt.domain.qa.QACompletionRequest;
 import com.prj.chatgpt.domain.qa.QACompletionResponse;
+import com.prj.chatgpt.domain.whisper.WhisperResponse;
 import io.reactivex.Single;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.http.*;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.util.Map;
 
 /**
  * @description Define the interface based on the ChatGPT official website API model.
@@ -32,4 +50,199 @@ public interface IOpenAiApi {
      */
     @POST(v1_chat_completions)
     Single<ChatCompletionResponse> completions(@Body ChatCompletionRequest chatCompletionRequest);
+
+    /**
+     * Text fix
+     *
+     * @param editRequest request information; parameters for editing text
+     * @return response result
+     */
+//    @POST("v1/edits") // This version was deprecated already
+    @POST(v1_chat_completions)
+    Single<EditResponse> edits(@Body EditRequest editRequest);
+
+    /**
+     * Generate picture
+     * curl https://api.openai.com/v1/images/generations \
+     * -H "Content-Type: application/json" \
+     * -H "Authorization: Bearer $OPENAI_API_KEY" \
+     * -d '{
+     * "prompt": "A cute baby sea otter",
+     * "n": 2,
+     * "size": "1024x1024"
+     * }'
+     * <p>
+     * {
+     * "created": 1589478378,
+     * "data": [
+     * {
+     * "url": "https://..."
+     * },
+     * {
+     * "url": "https://..."
+     * }
+     * ]
+     * }
+     *
+     * @param imageRequest image object
+     * @return response result
+     */
+    @POST("v1/images/generations")
+    Single<ImageResponse> genImages(@Body ImageRequest imageRequest);
+
+    /**
+     * Modify pictures
+     * <p>
+     * curl https://api.openai.com/v1/images/edits \
+     * -H "Authorization: Bearer $OPENAI_API_KEY" \
+     * -F image="@otter.png" \
+     * -F mask="@mask.png" \
+     * -F prompt="A cute baby sea otter wearing a beret" \
+     * -F n=2 \
+     * -F size="1024x1024"
+     * <p>
+     * {
+     * "created": 1589478378,
+     * "data": [
+     * {
+     * "url": "https://..."
+     * },
+     * {
+     * "url": "https://..."
+     * }
+     * ]
+     * }
+     *
+     * @param image image object
+     * @param mask picture object
+     * @param requestBodyMap request parameters
+     * @return response result
+     */
+    @Multipart
+    @POST("v1/images/edits")
+    Single<ImageResponse> editImages(@Part MultipartBody.Part image, @Part MultipartBody.Part mask, @PartMap Map<String, RequestBody> requestBodyMap);
+
+    /**
+     * Vector calculation
+     * curl https://api.openai.com/v1/images/variations \
+     * -H "Authorization: Bearer $OPENAI_API_KEY" \
+     * -F image="@otter.png" \
+     * -F n=2 \
+     * -F size="1024x1024"
+     *
+     * @param embeddingRequest request object
+     * @return response result
+     */
+    @POST("v1/embeddings")
+    Single<EmbeddingResponse> embeddings(@Body EmbeddingRequest embeddingRequest);
+
+    /**
+     * File list, after you upload the file to the server, you can get the list information
+     * curl https://api.openai.com/v1/files \
+     * -H "Authorization: Bearer $OPENAI_API_KEY"
+     *
+     * @return response result
+     */
+    @GET("v1/files")
+    Single<OpenAiResponse<File>> files();
+
+    /**
+     * Upload File: Uploads a file containing documentation to be used in various endpoints/functions. Currently, all files uploaded by an organization can be up to 1GB in size. If you need to increase the storage limit, please contact the official website.
+     * curl https://api.openai.com/v1/files \
+     * -H "Authorization: Bearer $OPENAI_API_KEY" \
+     * -F purpose="fine-tune" \
+     * -F file="@mydata.jsonl"
+     *
+     * @param file file
+     * @param purpose "fine-tune"
+     * @return response result
+     */
+    @Multipart
+    @POST("v1/files")
+    Single<UploadFileResponse> uploadFile(@Part MultipartBody.Part file, @Part("purpose") RequestBody purpose);
+
+    /**
+     * Delete Files
+     * curl https://api.openai.com/v1/files/file-XjGxS3KTG0uNmNOK362iJua3 \
+     * -X DELETE \
+     * -H "Authorization: Bearer $OPENAI_API_KEY"
+     *
+     * @param fileId file ID
+     * @return response result
+     */
+    @DELETE("v1/files/{file_id}")
+    Single<DeleteFileResponse> deleteFile(@Path("file_id") String fileId);
+
+    /**
+     * Retrieve files
+     * curl https://api.openai.com/v1/files/file-XjGxS3KTG0uNmNOK362iJua3 \
+     * -H "Authorization: Bearer $OPENAI_API_KEY"
+     *
+     * @param fileId file ID
+     * @return response result
+     */
+    @GET("v1/files/{file_id}")
+    Single<File> retrieveFile(@Path("file_id") String fileId);
+
+    /**
+     * Retrieve file content information
+     * curl https://api.openai.com/v1/files/file-XjGxS3KTG0uNmNOK362iJua3/content \
+     * -H "Authorization: Bearer $OPENAI_API_KEY" > file.jsonl
+     *
+     * @param fileId file ID
+     * @return response result
+     */
+    @Streaming
+    @GET("v1/files/{file_id}/content")
+    Single<ResponseBody> retrieveFileContent(@Path("file_id") String fileId);
+
+    /**
+     * Convert speech to text
+     * curl https://api.openai.com/v1/audio/transcriptions \
+     * -H "Authorization: Bearer $OPENAI_API_KEY" \
+     * -H "Content-Type: multipart/form-data" \
+     * -F file="@/path/to/file/audio.mp3" \
+     * -F model="whisper-1"
+     *
+     * @param file voice file
+     * @param requestBodyMap request information
+     * @return response result
+     */
+    @Multipart
+    @POST("v1/audio/transcriptions")
+    Single<WhisperResponse> speed2TextTranscriptions(@Part MultipartBody.Part file, @PartMap() Map<String, RequestBody> requestBodyMap);
+
+    /**
+     * Voice translation
+     * curl https://api.openai.com/v1/audio/translations \
+     * -H "Authorization: Bearer $OPENAI_API_KEY" \
+     * -H "Content-Type: multipart/form-data" \
+     * -F file="@/path/to/file/german.m4a" \
+     * -F model="whisper-1"
+     *
+     * @param file voice file
+     * @param requestBodyMap request information
+     * @return response result
+     */
+    @Multipart
+    @POST("v1/audio/translations")
+    Single<WhisperResponse> speed2TextTranslations(@Part MultipartBody.Part file, @PartMap() Map<String, RequestBody> requestBodyMap);
+
+    /**
+     * Bill query
+     *
+     * @return response result
+     */
+    @GET("v1/dashboard/billing/subscription")
+    Single<Subscription> subscription();
+
+    /**
+     * Consumption query
+     *
+     * @param starDate start time
+     * @param endDate end time
+     * @return response data
+     */
+    @GET("v1/dashboard/billing/usage")
+    Single<BillingUsage> billingUsage(@Query("start_date") LocalDate starDate, @Query("end_date") LocalDate endDate);
 }
--- src/test/java/com/prj/chatgpt/test/ApiTest.java
+++ src/test/java/com/prj/chatgpt/test/ApiTest.java
@@ -3,9 +3,20 @@ package com.prj.chatgpt.test;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prj.chatgpt.common.Constants;
+import com.prj.chatgpt.domain.billing.BillingUsage;
+import com.prj.chatgpt.domain.billing.Subscription;
 import com.prj.chatgpt.domain.chat.ChatCompletionRequest;
 import com.prj.chatgpt.domain.chat.ChatCompletionResponse;
 import com.prj.chatgpt.domain.chat.Message;
+import com.prj.chatgpt.domain.edits.EditRequest;
+import com.prj.chatgpt.domain.edits.EditResponse;
+import com.prj.chatgpt.domain.embedd.EmbeddingResponse;
+import com.prj.chatgpt.domain.files.DeleteFileResponse;
+import com.prj.chatgpt.domain.files.UploadFileResponse;
+import com.prj.chatgpt.domain.images.ImageEnum;
+import com.prj.chatgpt.domain.images.ImageRequest;
+import com.prj.chatgpt.domain.images.ImageResponse;
+import com.prj.chatgpt.domain.other.OpenAiResponse;
 import com.prj.chatgpt.domain.qa.QACompletionRequest;
 import com.prj.chatgpt.domain.qa.QACompletionResponse;
 import com.prj.chatgpt.session.Configuration;
@@ -18,6 +29,10 @@ import okhttp3.sse.EventSourceListener;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.concurrent.CountDownLatch;
 
@@ -33,9 +48,11 @@ public class ApiTest {
     public void test_OpenAiSessionFactory() {
         //1.Configure file
         Configuration configuration = new Configuration();
-//        configuration.setApiHost("https://api.openai-proxy.com/");
-        configuration.setApiHost("https://api.openai.com/");
-        configuration.setApiKey("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");
+//        configuration.setApiHost("https://service-d6wuqy4n-1320869466.cd.apigw.tencentcs.com/");//From xfg
+        configuration.setApiHost("https://api.openai.com/");//Official
+//        configuration.setApiHost("https://api.openai-proxy.com/");//Proxy of official
+//        configuration.setApiKey("sk-xxxxx");//From xfg key
+        configuration.setApiKey("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");//Official key
         // 测试时候，需要先获得授权token：http://api.xfg.im:8080/authorize?username=xfg&password=123 - 此地址暂时有效，后续根据课程首页说明获取token；https://t.zsxq.com/0d3o5FKvc
         //configuration.setAuthToken("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ4ZmciLCJleHAiOjE2ODMyODE2NzEsImlhdCI6MTY4MzI3ODA3MSwianRpIjoiMWUzZTkwYjYtY2UyNy00NzNlLTk5ZTYtYWQzMWU1MGVkNWE4IiwidXNlcm5hbWUiOiJ4ZmcifQ.YgQRJ2U5-9uydtd6Wbkg2YatsoX-y8mS_OJ3FdNRaX0");
         // 2. Session factory
@@ -121,4 +138,155 @@ public class ApiTest {
         new CountDownLatch(1).await();
     }
 
+    /**
+     * Continuous contextual conversation function
+     */
+    @Test
+    public void test_chat_completions_context() {
+        // 1-1. Create parameters
+        ChatCompletionRequest chatCompletion = ChatCompletionRequest
+                .builder()
+                .messages(new ArrayList<>())
+                .model(ChatCompletionRequest.Model.GPT_4.getCode())
+                .user("testUser01")
+                .build();
+        // Write in the request information
+        chatCompletion.getMessages().add(Message.builder().role(Constants.Role.USER).content("写一个java冒泡排序").build());
+
+        // 1-2. Ask for request
+        ChatCompletionResponse chatCompletionResponse01 = openAiSession.completions(chatCompletion);
+        log.info("Tested result：{}", chatCompletionResponse01.getChoices());
+
+        // Continue writing in the request information
+        chatCompletion.getMessages().add(Message.builder().role(Constants.Role.USER).content(chatCompletionResponse01.getChoices().get(0).getMessage().getContent()).build());
+        chatCompletion.getMessages().add(Message.builder().role(Constants.Role.USER).content("换一种写法").build());
+
+        ChatCompletionResponse chatCompletionResponse02 = openAiSession.completions(chatCompletion);
+        log.info("Tested result：{}", chatCompletionResponse02.getChoices());
+    }
+
+    /**
+     * Text edit: This model seems un-work right now, just use the gpt-4 chat instead.
+     */
+    @Test
+    public void test_edit() {
+        // Text request
+        EditRequest textRequest = EditRequest.builder()
+                .input("请问现在是基点")
+                .instruction("帮我修改错字")
+                .model(EditRequest.Model.TEXT_DAVINCI_EDIT_001.getCode()).build();
+        EditResponse textResponse = openAiSession.edit(textRequest);
+        log.info("Tested result：{}", textResponse);
+
+        // Code request
+        EditRequest codeRequest = EditRequest.builder()
+                // j <= 10 Shouble be i <= 10
+                .input("for (int i = 1; j <= 10; i++) {\n" +
+                        "    System.out.println(i);\n" +
+                        "}")
+                .instruction("这段代码执行时报错，请帮我修改").model(EditRequest.Model.CODE_DAVINCI_EDIT_001.getCode()).build();
+        EditResponse codeResponse = openAiSession.edit(codeRequest);
+        log.info("Tested result：{}", codeResponse);
+    }
+
+    /**
+     * Image generation
+     */
+    @Test
+    public void test_genImages() {
+        // Method1: Simple invoke
+        ImageResponse imageResponse01 = openAiSession.genImages("画一个小丑");
+        log.info("Tested result：{}", imageResponse01);
+
+        // Method2: set parameters invoke
+        ImageResponse imageResponse02 = openAiSession.genImages(ImageRequest.builder()
+                .prompt("画一个小丑")
+                .size(ImageEnum.Size.size_256.getCode())
+                .responseFormat(ImageEnum.ResponseFormat.B64_JSON.getCode()).build());
+        log.info("Tested result：{}", imageResponse02);
+    }
+
+    /**
+     * Edit image，three different methods with different parameters
+     */
+    @Test
+    public void test_editImages() throws IOException {
+        ImageResponse imageResponse = openAiSession.editImages(new File("E:/Codes/JavaBeginner/Projects/chatgpt-sdk-java_v2/docs/images/2.png"), "把鼻子涂成红色”");
+        log.info("Tested result：{}", imageResponse);
+    }
+
+    @Test
+    public void test_embeddings() {
+        EmbeddingResponse embeddingResponse = openAiSession.embeddings("哈喽", "嗨", "hi!");
+        log.info("Tested result：{}", embeddingResponse);
+    }
+
+    @Test
+    public void test_files() {
+        OpenAiResponse<File> openAiResponse = openAiSession.files();
+        log.info("Tested result：{}", openAiResponse);
+    }
+
+    @Test
+    public void test_uploadFile() {
+        UploadFileResponse uploadFileResponse = openAiSession.uploadFile(new File("E:/Codes/JavaBeginner/Projects/chatgpt-sdk-java_v2/docs/files/introduction.jsonl"));
+        log.info("Tested result：{}", uploadFileResponse);
+    }
+
+    /**
+     * The fileid needs to be obtained after uploading the file in the previous step.
+     * Last step response information: {
+     *   "object": "file",
+     *   "id": "file-vIPelH3ppYbwasHfM9iBBH6a",
+     *   "purpose": "fine-tune",
+     *   "filename": "introduction.jsonl",
+     *   "bytes": 248,
+     *   "created_at": 1706522584,
+     *   "status": "processed",
+     *   "status_details": null
+     * }
+     * ---------------------------------
+     * This step response information: {
+     *   "object": "file",
+     *   "id": "file-vIPelH3ppYbwasHfM9iBBH6a",
+     *   "deleted": true
+     * }
+     */
+    @Test
+    public void test_deleteFile() {
+        DeleteFileResponse deleteFileResponse = openAiSession.deleteFile("file-vIPelH3ppYbwasHfM9iBBH6a");//file id 上传后才能获得
+        log.info("Tested resultn：{}", deleteFileResponse);
+    }
+
+    /**
+     * Response: {
+     *   "error": {
+     *     "message": "Your request to GET /v1/dashboard/billing/subscription must be made with a session key (that is, it can only be made from the browser). You made it with the following key type: secret.",
+     *     "type": "server_error",
+     *     "param": null,
+     *     "code": null
+     *   }
+     * }
+     */
+    @Test
+    public void test_subscription() {
+        Subscription subscription = openAiSession.subscription();
+        log.info("Tested result：{}", subscription);
+    }
+
+    /**
+     * Response: {
+     *   "error": {
+     *     "message": "Your request to GET /v1/dashboard/billing/usage must be made with a session key (that is, it can only be made from the browser). You made it with the following key type: secret.",
+     *     "type": "server_error",
+     *     "param": null,
+     *     "code": null
+     *   }
+     * }
+     */
+    @Test
+    public void test_billingUsage() {
+        BillingUsage billingUsage = openAiSession.billingUsage(LocalDate.of(2023, 3, 20), LocalDate.now());
+        log.info("Tested result：{}", billingUsage.getTotalUsage());
+    }
 }
